f90a77c79420ca3b79e413747d8cb32d56213f85
alexpineda99 <alexandropinedam1310@gmail.com>
2025-04-22 17:10:46 -0400

    vite añadido

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store, persistor } from "./State/store"
-import {PersistGate} from "redux-persist/integration/react"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./State/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 import "./Assets/Css/Main.css";
-import 'react-lazy-load-image-component/src/effects/blur.css';
-
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+        ,
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#222222',
+    },
+    secondary: {
+      main: '#109DFA',
+    },
+    error: {
+      main: '#CF142B',
+    },
+  },
+});
